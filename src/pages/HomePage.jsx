37ffe1032a62018172beb5cb9b061c1e56a6cf1a
Rashijain2709 <rashij2709@gmail.com>
2025-08-21 18:52:57 +0530

    Some UI changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div className="homepage-container">
       <h1>Welcome to Movie Watchlist!</h1>
-      <p>You can also search for new movies to add to your watchlist!</p>
+      <p>You can also search for new movies and add to your watchlist!</p>
     </div>
   );
 }
